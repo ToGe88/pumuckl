274f774c60381fec5b4cfa56ae80749facd45f04
TG <tg@TGs-MacBook-Pro.fritz.box>
2018-06-23 17:17:06 +0200

    Navigation Grid CSS

--- site/snippets/generals/menu.php
+++ site/snippets/generals/menu.php
@@ -1,11 +1,16 @@
 <div class="menu">
 	<div class="menu__holder grid__holder">
-		<? $menuItems = new Pages; ?>
-		<? $menuItems = $menuItems->add($page->moduleList()->filterBy('template', 'in', ['module.__text', 'module.__item-overview'])); ?>
-		<? foreach($menuItems as $menuItem): ?>
-			<div class="menu__item">
-				<a href="<?= '#'.$menuItem->title()->lower() ?>"><?= $menuItem->title() ?></a>
-			</div>
-		<? endforeach ?>
+		<div class="logo grid--span-3">
+			<img src="<?= $site->images()->first()->url() ?>" width="200" />
+		</div>
+		<div class="anchors__holder grid__holder grid--span-4 grid--end-13">
+			<? $menuItems = new Pages; ?>
+			<? $menuItems = $menuItems->add($page->moduleList()->filterBy('template', 'in', ['module.__text', 'module.__item-overview'])); ?>
+			<? foreach($menuItems as $menuItem): ?>
+				<div class="menu__item grid--span-6">
+					<a href="<?= '#'.$menuItem->title()->lower() ?>"><?= $menuItem->title() ?></a>
+				</div>
+			<? endforeach ?>
+		</div>
 	</div>
 </div>
