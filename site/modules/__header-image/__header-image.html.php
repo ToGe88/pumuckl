c2ee425ca7ae09a80432c23f0ce14e68d71cb441
TG <tg@TGs-MacBook-Pro.fritz.box>
2018-06-24 22:01:02 +0200

    fertig ohne Mobile

--- site/modules/__header-image/__header-image.html.php
+++ site/modules/__header-image/__header-image.html.php
@@ -1,7 +1,7 @@
 <? $m = $module ?>
 
 <section class="header-image__wrapper wrapper grid--span-12">
-	<div class="header-image__holder">
+	<div class="header-image__holder grid__holder">
 		<? if($m->hero()->isNotEmpty()): ?>
 			<? if($m->hero()->toFile()->type() === 'image'): ?>
 	  			<? snippet('images/figure', array('backgroundImage' => true, 'heightClass' => 'height--full', 'm' => $m, 'file' => $m->hero()->toFile(), 'maxWidth' => 3200, 'maxHeight' => 3200, 'randWidth' => false, 'objectCover' => true)) ?>
--- site/templates/login.php
+++ site/templates/login.php
@@ -1,19 +1,26 @@
-<?php snippet('header') ?>
+<?php snippet('generals/header') ?>
 
-<h1><?= $page->title()->html() ?></h1>
 
-<?php if($error): ?>
-<div class="alert"><?= $page->alert()->html() ?></div>
-<?php endif ?>
 
-<form method="post">
-  <div>
-    <label for="password"><?= $page->password()->html() ?></label>
-    <input type="password" id="password" name="password">
+<section class="form__wrapper grid__wrapper grid--span-8 grid--start-3">
+  <div class="grid__holder grid--height-50vh" style="margin-top: 100px">
+    <div class="grid--span-12">
+      <h2>Logge dich ein. Nutze dafür den Zugangscode von Instagram!</h2>
+    </div>
+    <div class="grid--span-12">
+      <?php if($error): ?>
+        <div><p class="alert"><?= $page->alert()->html() ?></p></div>
+      <?php endif ?>
+      <form method="post">
+        <div class="grid--span-12">
+          <input type="password" id="password" name="password">
+        </div>
+        <div>
+          <input type="submit" name="login" value="<?= $page->button()->html() ?>">
+        </div>
+      </form>
+    </div>
   </div>
-  <div>
-    <input type="submit" name="login" value="<?= $page->button()->html() ?>">
-  </div>
-</form>
+</section>
 
-<?php snippet('footer') ?>
+<?php snippet('generals/footer') ?>
